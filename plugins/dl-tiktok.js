2d4365cff01d955c36bb2a470bcd1e48b7b65606
malgtps12 <93052305+malgtps12@users.noreply.github.com>
2022-09-04 23:58:48 +0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -9,7 +9,7 @@ if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/
 *Nickname:* ${nickname}
 *Description:* ${description}
 
-_©BotzArull_
+_©MallBot-MD_
 `.trim(), m)
 }
 handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
